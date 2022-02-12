f7b28cd143c750042eb1bb41f867ab96bf0a55e2
classmatewu <171755422@qq.com>
2022-02-12 23:46:54 +0800

    :bookmark:upd(application): 更新nativeHandleRequest调用

--- packages/toy-koa/lib/application.js
+++ packages/toy-koa/lib/application.js
@@ -10,6 +10,9 @@ const respose = require('./response')
 class Koa {
   constructor() {
     this.fn = null
+    this.context = context
+    this.request = request
+    this.respose = respose
   }
 
   use(fn) {
@@ -21,7 +24,7 @@ class Koa {
   }
 
   listen(...args) {
-    const server = http.createServer(this.fn)
+    const server = http.createServer(this.nativeHandleRequest.bind(this)) // 注意这种传函数表达式的，函数有this调用的，最好bind一下，不然this可能会丢失，因为this的取值是函数在调用时确定的
     server.listen(...args)
   }
 }
